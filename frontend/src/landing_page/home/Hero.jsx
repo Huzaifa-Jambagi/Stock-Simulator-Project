ea1362f2a7f1a1fd2e80c8ff6caa0535bea611d2
Huzaifa-Jambagi <166741382+Huzaifa-Jambagi@users.noreply.github.com>
2025-05-01 01:25:01 +0530

    Update Hero.jsx

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     <div className='container p-4'>
       <div className="row text-center">
         <img src='assets/homeHero.png' className='mb-5 img-fluid' />
-        <h1 className='mt-4'>Invest in everything</h1>
-        <h5 className='mt-2'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</h5>
+        <h1 className='mt-4'>Invest in virtual stocks</h1>
+        <h5 className='mt-2'>Simulated Trading – Practice trading stocks without real money.</h5>
         <Link style={{textDecoration:'none'}} to='/register'>
         <div className="d-flex justify-content-center mt-3">
           <button className="btn btn-success fw-bold text-white w-50 w-md-25 mb-3" style={{  maxWidth: "200px",backgroundColor: "#027a5c", border: "none" }}>
